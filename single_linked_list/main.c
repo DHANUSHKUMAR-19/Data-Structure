c329f53d016f5b1a5fd0e1d9efa6f79cb63b432e
Dhanush Kumar <85663405+DHANUSHKUMAR-19@users.noreply.github.com>
2022-03-07 12:00:38 +0530

    reverse function added

--- single_linked_list/main.c
+++ single_linked_list/main.c
@@ -196,7 +196,7 @@ int main()
    for(;;){
         printf("------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
     printf("\nEnter your choice\n");
-    printf("\n1.Insert\n2.Delete\n3.Display\n4.exit\n5.Check length\n6.Alternate node printing\n7,Insert at after specific element\n8.Search\n");
+    printf("\n1.Insert\n2.Delete\n3.Display\n4.exit\n5.Check length\n6.Alternate node printing\n7,Insert at after specific element\n8.Search\n9.Reverse list\n");
     scanf("%d",&ch);
     switch(ch){
     case 1:printf("Enter the elemnt to insert\n");
@@ -218,6 +218,7 @@ int main()
     case 8:search(first);
     break;
     case 9:first=revrs(first);
+    break;
     }
    }
     return 0;
